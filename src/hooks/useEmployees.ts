e0899629b93ee1cfa17169bac36c1dc1b4c36a90
River Mu <xmu>
2024-08-03 21:29:50 +0800

    first commit

---
+++ src/hooks/useEmployees.ts
@@ -0,0 +1,20 @@
+import { useCallback, useState } from "react"
+import { Employee } from "../utils/types"
+import { useCustomFetch } from "./useCustomFetch"
+import { EmployeeResult } from "./types"
+
+export function useEmployees(): EmployeeResult {
+  const { fetchWithCache, loading, fetchWithoutCache } = useCustomFetch()
+  const [employees, setEmployees] = useState<Employee[] | null>(null)
+
+  const fetchAll = useCallback(async () => {
+    const employeesData = await fetchWithoutCache<Employee[]>("employees")
+    setEmployees(employeesData)
+  }, [fetchWithoutCache])
+
+  const invalidateData = useCallback(() => {
+    setEmployees(null)
+  }, [])
+
+  return { data: employees, loading, fetchAll, invalidateData }
+}
